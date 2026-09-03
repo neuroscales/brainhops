597ee1525e2db5dd2daf29fc46fc563e26c65d78
James Jay Scherick <js1518@thallo.nmr.mgh.harvard.edu>
2026-09-03 15:18:33 -0400

    added tests

---
+++ tests/test_fsl.py
@@ -0,0 +1,16 @@
+import pytest
+from brainhops.io.transformations.fsl.displacement._xforms import FslDisplacementTransformation
+
+
+def test_fsl_coef() -> None:
+    transformation = FslDisplacementTransformation.from_file(
+        "tests/data/fsl_coef.nii.gz")
+    assert (len(transformation.transformations) == 3)
+    assert (transformation.transformations[1].is_spline_coefficients)
+
+
+def test_fsl_dense() -> None:
+    transformation = FslDisplacementTransformation.from_file(
+        "tests/data/fsl_field.nii.gz")
+    assert (len(transformation.transformations) == 3)
+    assert (not transformation.transformations[1].is_spline_coefficients)
